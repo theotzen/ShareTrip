793b2ad9220a8701bc349df830a3b58dc1ca628c
Theodore Otzenberger <totzenberger@palantir.com>
2023-01-19 17:36:22 +0100

    first commit

--- app/database.py
+++ app/database.py
@@ -5,6 +5,8 @@ from app.core.log_config import init_loggers
 
 loggerIH = init_loggers(__name__)
 
+loggerIH.warn(settings.DATABASE_URL)
+
 client = mongo_client.MongoClient(
     settings.DATABASE_URL, serverSelectionTimeoutMS=5000)
 
@@ -12,7 +14,7 @@ try:
     conn = client.server_info()
     loggerIH.info(f'Connected to MongoDB {conn.get("version")}')
 except Exception:
-    loggerIH.exception("Unable to connect to the MongoDB server.")
+    loggerIH.exception("Unable to connect to the MongoDB server.", Exception.args)
 
 db = client[settings.MONGO_INITDB_DATABASE]
 User = db.users
