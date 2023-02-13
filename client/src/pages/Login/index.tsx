5afa10a471cfdcf15241a9367ff7bd8d8296f8e4
Theodore Otzenberger <totzenberger@palantir.com>
2023-02-13 19:10:11 +0100

    nit

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -3,7 +3,6 @@ import { useIntl } from 'react-intl';
 import LoginForm from '../../components/LoginForm';
 import RegisterForm from '../../components/RegisterForm';
 import * as styles from './style';
-import React from 'react';
 
 
 export default function Login() {
