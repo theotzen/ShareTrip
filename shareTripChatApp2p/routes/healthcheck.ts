aa2970a295e881d6799366e3658f40e2b615092a
Theodore Otzenberger <totzenberger@palantir.com>
2023-03-19 12:04:32 +0100

    renaming

--- shareTripChatApp2p/routes/healthcheck.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router()
-
-router.get('/beat', (req: express.Request, res: express.Response) => {
-        res.status(201).json({message: 'Living in SHARETRIP'})
-    })
-
-module.exports = router
--- shareTripChatApp2p/utils/expressSchema.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-
-export interface TypedRequestBody<T> extends express.Request {
-    body: T
-}
